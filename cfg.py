30cd3e7aeda8e19de17bde9b46f231e217ccc47b
Anthagonas <the_rod_21@hotmail.com>
2017-10-05 23:06:08 +0200

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,8 @@
+# cfg.py
+HOST = "irc.twitch.tv"              # the Twitch IRC server
+PORT = 6667                         # always use port 6667!
+NICK = "anthagonas"            # your Twitch username, lowercase
+NICK1 = "anthagonus"
+NICK2 = "melikespam"
+
+SPAM_TRESHOLD = 4                       # nb of identical message before you spam 
